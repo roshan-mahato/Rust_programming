c58d6fe00d570cd4a6d450f21f4ce5f79d6d813f
roshan-mahato <71814556+roshan-mahato@users.noreply.github.com>
2022-09-22 11:43:44 +0545

    new file:   hello_world/Cargo.lock 	new file:   hello_world/Cargo.toml 	new file:   hello_world/src/main.rs 	new file:   hello_world/target/.rustc_info.json 	new file:   hello_world/target/CACHEDIR.TAG 	new file:   hello_world/target/release/.cargo-lock 	new file:   hello_world/target/release/.fingerprint/hello_world-501f8a960080e9eb/bin-hello_world 	new file:   hello_world/target/release/.fingerprint/hello_world-501f8a960080e9eb/bin-hello_world.json 	new file:   hello_world/target/release/.fingerprint/hello_world-501f8a960080e9eb/dep-bin-hello_world 	new file:   hello_world/target/release/.fingerprint/hello_world-501f8a960080e9eb/invoked.timestamp 	new file:   hello_world/target/release/deps/hello_world.d 	new file:   hello_world/target/release/deps/hello_world.exe 	new file:   hello_world/target/release/deps/hello_world.pdb 	new file:   hello_world/target/release/hello_world.d 	new file:   hello_world/target/release/hello_world.exe 	new file:   hello_world/target/release/hello_world.pdb

---
+++ hello_world/target/release/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\NITRO\Documents\GitHub\Rust_programming\hello_world\target\release\deps\hello_world.exe: src\main.rs
+
+C:\Users\NITRO\Documents\GitHub\Rust_programming\hello_world\target\release\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\NITRO\Documents\GitHub\Rust_programming\hello_world\target\release\hello_world.exe: C:\Users\NITRO\Documents\GitHub\Rust_programming\hello_world\src\main.rs
